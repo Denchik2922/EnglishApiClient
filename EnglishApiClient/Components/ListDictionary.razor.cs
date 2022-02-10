8a7800bd7e075f93aab8ed72c0eb13b4beb885a6
ELEKS-SOFTWARE\denys.yavor <iavordenis@gmail.com>
2022-02-10 16:22:53 +0200

    Added loading indicator and refactoring dictionary pages

--- EnglishApiClient/Components/ListDictionary.razor.cs
+++ EnglishApiClient/Components/ListDictionary.razor.cs
@@ -1,4 +1,6 @@
 ﻿using EnglishApiClient.Dtos.Entity;
+using EnglishApiClient.HttpServices.Interfaces;
+using EnglishApiClient.Infrastructure.RequestFeatures;
 using Microsoft.AspNetCore.Components;
 
 namespace EnglishApiClient.Components
@@ -7,6 +9,59 @@ namespace EnglishApiClient.Components
     {
 
         [Parameter]
-        public ICollection<EnglishDictionary> Dictionaries { get; set; }
+        public bool IsPrivateDictionaries { get; set; }
+
+        public ICollection<EnglishDictionary> Dictionaries;
+        public MetaData MetaData { get; set; } = new MetaData();
+        private PaginationParameters parameters = new PaginationParameters() { PageSize = 5 };
+
+        private Dictionary<string, string> _sortTypes = new Dictionary<string, string>()
+        {
+            {"Name", "name" },
+            {"Name DESC", "name desc" }
+        };
+
+        [Inject]
+        private IDictionaryHttpService _dictionaryService { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await GetDictionaries();
+        }
+
+        private async Task GetDictionaries()
+        {
+            PagingResponse<EnglishDictionary> pagingResponse;
+            if(IsPrivateDictionaries == true)
+            {
+                pagingResponse = await _dictionaryService.GetPrivateDictionaries(parameters);
+            }
+            else
+            {
+                pagingResponse = await _dictionaryService.GetPublicDictionaries(parameters);
+            }
+            Dictionaries = pagingResponse.Items;
+            MetaData = pagingResponse.MetaData;
+        }
+
+        private async Task SearchChanged(SearchParameters searchParameters)
+        {
+            parameters.PageNumber = 1;
+            parameters.SearchParameters = searchParameters;
+            await GetDictionaries();
+        }
+
+        private async Task SortChanged(string orderBy)
+        {
+            Console.WriteLine(orderBy);
+            parameters.OrderBy = orderBy;
+            await GetDictionaries();
+        }
+
+        private async Task SelectedPage(int page)
+        {
+            parameters.PageNumber = page;
+            await GetDictionaries();
+        }
     }
 }
